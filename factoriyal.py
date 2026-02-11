b7dc76be5698421ab1b0585580334db179aa8787
SUMIT VAGHELA <sumitvaghela175@gmail.com>
2026-02-11 18:11:00 +0530

    Add files via upload

---
+++ factoriyal.py
@@ -0,0 +1,7 @@
+#WAP to print the factorial of given number.
+
+n = int(input("Enter a number: "))
+fact = 1
+for i in range(1, n + 1):
+    fact *= i
+print(f"Factorial of {n} is {fact}")
---
+++ primeSeries.py
@@ -0,0 +1,11 @@
+#WAP to print prime number series between 1 to 50.
+
+for num in range(2, 51):
+    is_prime = True
+    for i in range(2, int(num ** 0.5) + 1):
+        if num % i == 0:
+            is_prime = False
+            break
+    if is_prime:
+        print(num, end=" ")
+print()
